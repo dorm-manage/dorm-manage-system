c54c280816275144c890d2072aa00ac2ec5d8b9f
shoval <shoval.p30@gmail.com>
2025-01-15 15:34:07 +0200

    index draft and other pages

--- DormitoriesPlus/urls.py
+++ DormitoriesPlus/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('application/', views.application, name='application'),
+    path('faults/', views.faults, name='faults'),
+    path('connect_us/', views.connect_us, name='connect_us'),
 ]
--- DormitoriesPlus/views.py
+++ DormitoriesPlus/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+
+def application(request):
+    return render(request, 'application.html')
+
+
+def faults(request):
+    return render(request, 'faults.html')
+
+
+def connect_us(request):
+    return render(request, 'connect_us.html')
