03964979b6516835cc4ed81ab526a5b6363e4d10
shoval <shoval.p30@gmail.com>
2025-01-24 18:02:29 +0200

    linkin all pages

--- DormitoriesPlus/urls.py
+++ DormitoriesPlus/urls.py
@@ -2,9 +2,28 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+
+    path('', views.Homepage, name='Homepage.html'),
     path('application/', views.application, name='application'),
     path('faults/', views.faults, name='faults'),
     path('connect_us/', views.connect_us, name='connect_us'),
     path('Personal_erea/', views.Personal_erea, name='personal_erea'),
+    path('manager_Homepage/', views.manager_Homepage, name='manager_Homepage'),
+    path('BM_Homepage/', views.BM_Homepage, name='BM_Homepage'),
+    path('Students_homepage/', views.Students_homepage, name='Students_homepage'),
+    path('BM_faults/', views.BM_faults, name='BM_faults'),
+    path('BM_sendMassage/', views.BM_sendMassage, name='BM_sendMassage'),
+    path('manage_room/', views.manage_room, name='manage_room'),
+    path('manager_inventory/', views.manager_inventory, name='manager_inventory'),
+    path('manager_BM/', views.manager_BM, name='manager_BM'),
+    path('Manager_request/', views.Manager_request, name='Manager_request'),
+    path('manager_faults/', views.manager_faults, name='manager_faults'),
+
+
+
+
+
+
+
+
 ]
--- DormitoriesPlus/views.py
+++ DormitoriesPlus/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-def index(request):
-    return render(request, 'index.html')
+
+def Homepage(request):
+    return render(request, 'Homepage.html')
 
 
 def application(request):
@@ -19,3 +20,46 @@ def connect_us(request):
 
 def Personal_erea(request):
     return render(request, 'Personal_erea.html')
+
+
+def manager_Homepage(request):
+    return render(request, 'manager_Homepage.html')
+
+
+def BM_Homepage(request):
+    return render(request, 'BM_Homepage.html')
+
+
+def Students_homepage(request):
+    return render(request, 'Students_homepage.html')
+
+
+def BM_faults(request):
+    return render(request, 'BM_faults.html')
+
+
+def BM_sendMassage(request):
+    return render(request, 'BM_sendMassage.html')
+
+
+def manage_room(request):
+    return render(request, 'manage_room.html')
+
+
+def manager_inventory(request):
+    return render(request, 'manager_inventory.html')
+
+
+def manager_BM(request):
+    return render(request, 'manager_BM.html')
+
+
+
+def Manager_request(request):
+    return render(request, 'Manager_request.html')
+
+
+
+def manager_faults(request):
+    return render(request, 'manager_faults.html')
+
